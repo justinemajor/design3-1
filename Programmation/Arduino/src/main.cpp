141d3d2277917719cb7518b93853fad5a2f5337a
Maxime Tousignant-Tremblay <Maxime.Tousignant-Tremblay.1@ulaval.ca>
2023-02-12 23:19:12 -0500

    Updated schematics and PCB

--- Programmation/Arduino/src/main.cpp
+++ Programmation/Arduino/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>                // Header file for Arduino functions
+#include <SPI.h>                    // Header file for SPI devices communication functions
 #include <Vbias.h>                  // Header file for voltage bias thermistor temperature functions
 // #include <Ibias.h>                  // Header file for current bias thermistor temperature functions
 
@@ -14,11 +15,29 @@ void setup() {
   Serial.begin(115200);             // Initialize serial communication
 
   // Configure pins as inputs
-  // pinMode(A0, INPUT);
   pinMode(A1, INPUT);
 
   // Configure pins as outputs
   pinMode(2, OUTPUT);
+
+  // Enable internal pullups for unused pins 
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);
+  pinMode(A3, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP),
 
 }
 
